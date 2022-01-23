13512e0cafdf8bbbd1da535527b8f7fbe45eb8ba
Brique Au Bob <briqueaubob@gmail.com>
2022-01-23 12:59:47 +0100

    fix crash app + add removal of task

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -3,16 +3,18 @@
         <div v-if="todo">
             <h2 class="font-semibold text-lg mb-5">{{ todo.name }}</h2>
             <div 
-                v-for="(task, i) in todo.tasks?.sort((a: Task, b: Task) => a.completed ? 1 : -1)" 
-                :key="i" 
-                class="bg-white text-black px-4 py-4 rounded-lg mb-4 transition ease-in duration-200 flex items-center gap-3 cursor-pointer" 
+                v-for="(task, id) in todo.tasks"
+                :key="id"
+                class="bg-white text-black px-4 py-4 rounded-lg mb-4 transition ease-in duration-200 flex items-center justify-between" 
                 :class="task.completed && 'line-through opacity-50'"
-                @click="complete(task)"
             >
-                <div class="border-2 border-gray-300 rounded-full w-6 h-6 flex justify-center items-center" :class="task.completed && 'bg-gray-300'">
-                    <img v-if="task.completed" src="/images/check.svg" class="w-1/2 h-1/2" />
+                <div class="flex items-center gap-3">
+                    <div class="border-2 border-gray-300 rounded-full w-6 h-6 flex justify-center items-center cursor-pointer" :class="task.completed && 'bg-gray-300'" @click="complete(task)">
+                        <img v-if="task.completed" src="/images/check.svg" class="w-1/2 h-1/2" />
+                    </div>
+                    {{ task.content }}
                 </div>
-                {{ task.content }}
+                <button @click="removeTask(id)" class="focus:outline-none"><img src="/images/cross.svg" class="w-4 h-4" /></button>
             </div>
             <form class="mt-8 flex items-center justify-between mb-12" @submit.prevent>
                 <input v-model="form.content" class="bg-neutral-800 rounded-xl px-5 py-3 focus:outline-none text-white w-full mr-2" placeholder="Add a task" @enter="createTask" />
@@ -34,14 +36,21 @@
     const route = useRoute()
     const todoStore = useTodoStore()
     const todo = todoStore.find(route.params.name as string);
+    todo?.tasks?.sort((a: Task, b: Task) => a.completed ? 1 : -1)
     const form = reactive({
         content: ''
     });
 
     function createTask() {
+        if(!form.content) return;
         todo?.tasks?.push({ content: form.content, completed: false });
         todoStore.save();
         form.content = '';
+    }
+
+    function removeTask(id: number) {
+        todo?.tasks?.splice(id, 1);
+        todoStore.save();
     }
 
     function complete(task: Task) {
